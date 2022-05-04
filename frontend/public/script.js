ef7ad6ad4fa89df8921e4c273bd160c96e820695
nikibzsl <bzslniki@gmail.com>
2022-05-04 09:51:24 +0200

    put counter in

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,3 +1,14 @@
+function countingUp() {
+    document.getElementById("to100").innerHTML=window.cntr;
+   if (window.cntr<100) {
+       window.cntr++;
+       setTimeout(function(){countingUp();},20);
+   } else {
+   }
+ }
+ window.cntr=0; 
+
+
 // insert the loading animation of the HTML
 const animationComponent = function () {
     return`
