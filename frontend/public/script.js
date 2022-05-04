8debd19579ba72f0c59d32a5290a1be98775d200
NemethSzegediJudit <szegedijudit.gyp@gmail.com>
2022-05-04 20:54:03 +0200

    auto format in script.js

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,14 +1,14 @@
 const animationComponent = function countingUp() {
-    document.getElementById("to100").innerHTML=window.cntr;
-   if (window.cntr<100) {
-       window.cntr++;
-       setTimeout(function(){countingUp();},45);
-   } else {
-   }
- }
- window.cntr=0; 
+    document.getElementById("to100").innerHTML = window.cntr;
+    if (window.cntr < 100) {
+        window.cntr++;
+        setTimeout(function () { countingUp(); }, 45);
+    } else {
+    }
+}
+window.cntr = 0;
 
-        
+
 /* // insert the loading animation of the HTML
 const animationComponent = function () {
     return`
@@ -16,36 +16,36 @@ const animationComponent = function () {
     `
 } */
 
- // insert the header of the HTML
- const headerComponent = function () {
-     return`
+// insert the header of the HTML
+const headerComponent = function () {
+    return `
      
      `
- }
+}
 
- // insert the main section of the HTML
- const sectionComponent = function () {
-     return`
+// insert the main section of the HTML
+const sectionComponent = function () {
+    return `
      
      `
-    }
-    
- // if we have time :) insert the footer section of the HTML
- const footerComponent = function () {
-     return `
+}
+
+// if we have time :) insert the footer section of the HTML
+const footerComponent = function () {
+    return `
      
      `
- }
+}
 
- const loadEvent = function () {
-     const to100Element = document.getElementById("to100");
-     const rootElement = document.getElementById("root");
+const loadEvent = function () {
+    const to100Element = document.getElementById("to100");
+    const rootElement = document.getElementById("root");
 
-     to100Element.insertAdjacentHTML( "beforebegin", animationComponent() );
-     rootElement.insertAdjacentHTML( "afterbegin", headerComponent() );
-     rootElement.insertAdjacentHTML( "afterbegin", sectionComponent() );
-     rootElement.insertAdjacentHTML( "afterbegin", footerComponent() );
+    to100Element.insertAdjacentHTML("beforebegin", animationComponent());
+    rootElement.insertAdjacentHTML("afterbegin", headerComponent());
+    rootElement.insertAdjacentHTML("afterbegin", sectionComponent());
+    rootElement.insertAdjacentHTML("afterbegin", footerComponent());
 
- }
+}
 
- window.addEventListener("load", loadEvent)
+window.addEventListener("load", loadEvent)
