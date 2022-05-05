5f6d4096df56e9797a91498eea3a0f81ea2e3c54
NemethSzegediJudit <szegedijudit.gyp@gmail.com>
2022-05-06 00:39:05 +0200

    finished the task

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,4 +1,4 @@
-const animationComponent = function countingUp() {
+function countingUp() {
     document.getElementById("to100-counter").innerHTML = window.cntr;
     if (window.cntr < 100) {
         window.cntr++;
@@ -56,17 +56,8 @@ function scrollmanagingtwo() {
     document.addEventListener('scroll', onscroll3);
 }
 
-
-
-/* let mouseCursor = document.querySelector('.cursor');
-
- window.addEventListener('mousemove',cursor)
-
- function cursor(e){
-     console.log(e);
- } */
-
-/* // insert the loading animation of the HTML
+/* 
+// insert the loading animation of the HTML
 const animationComponent = function () {
 return`
  
@@ -74,23 +65,95 @@ return`
 } */
 
 // insert the header of the HTML
-const headerComponent = function () {
+const animationSectionomponent = function () {
     return `
-     
+    <div class="cursor"></div>
+	<!--Loading(Niki)-->
+	<div class="animation-container">
+		<div id="to100">
+			<img src="img/apostrophe.png">
+			<h1><span id="to100-counter">00</span></h1>
+		</div>
+		<div class="image-container">
+	
+			<img class="opening-image1" src="img/blackpattern.jpeg" alt="">
+			<img class="opening-image2" src="img/whitepattern.jpeg" alt="">
+
+		</div>
+		<div>
+			<span>Art objects</span>
+			<span>New York, NY</span>
+		</div>
+	</div>
      `
 }
 
 // insert the main section of the HTML
-const sectionComponent = function () {
+const firstSectionComponent = function () {
     return `
-     
+    <!-- 1st viewport - the menu (Gabor)-->
+
+	<nav class="menu-row-container">
+		<button class="section-name menu underline">MENU</button>
+		<p class="section-name menu-owner">MATTHEW FISHER</p>
+		<a href="#" class="section-name menu underline">CART</a>
+	</nav>
+
+	<div class="menu-screen">
+
+		<div class="art-objects">
+			<h1>ART</h1>
+			<h1>OBJECTS</h1>
+		</div>
+
+		<div class="left-column-container">
+			<a href="#" class="section-name side-list underline">INTRO</a>
+			<a href="#" class="section-name side-list underline">ABOUT</a>
+			<a href="#" class="section-name side-list underline">FEATURED</a>
+		</div>
+
+		<div class="menu-bottom">
+			<a href="#"><img src="img/dots.svg" alt="dotted menu"></a>
+			<a href="#" class="section-name side-list underline">INDEX</a>
+		</div>
+
+	</div>
      `
 }
 
-// if we have time :) insert the footer section of the HTML
-const footerComponent = function () {
+const secondSectionComponent = function () {
     return `
-     
+    <section id="two">
+        <div class="left">
+            <h2 class="section-name">apol</h2>
+            <img src="img/leftplant.jpg">
+        </div>
+        <div class="right">
+            <h2 class="section-name">hadr</h2>
+            <img src="img/rightplant.jpg">
+        </div>
+    </section>
+     `
+}
+
+const thirdSectionComponent = function () {
+    return `
+    <section id="three">
+        <div>
+            <h2 class="section-name about-title">About</h2>
+            <p class="about-text">
+                <span class="first-line">Existing between the</span>
+                <span>boundaries of nature and culture,</span>
+                <span>Matthew’s work conveys an evocative</span>
+                <span>nostalgia through the use of historically</span>
+                <span>revered natural materials. His work</span>
+                <span>ranges from unique art objects to</span>
+                <span>editioned productions and celebrates</span>
+                <span>the union between heritage, natural</span>
+                <span>materials, and refined craftsmanship.</span>
+            </p>
+        </div>
+    </section>
      `
 }
 
@@ -99,12 +162,13 @@ const loadEvent = function () {
     const rootElement = document.getElementById("root");
 
     /*to100Element.insertAdjacentHTML( "beforebegin", animationComponent() ); ez sem kell*/
-    animationComponent();
-
-    rootElement.insertAdjacentHTML("afterbegin", headerComponent());
-    rootElement.insertAdjacentHTML("afterbegin", sectionComponent());
-    rootElement.insertAdjacentHTML("afterbegin", footerComponent());
-
+    
+    rootElement.insertAdjacentHTML("beforeend", animationSectionomponent());
+    rootElement.insertAdjacentHTML("beforeend", firstSectionComponent());
+    rootElement.insertAdjacentHTML("beforeend", secondSectionComponent());
+    rootElement.insertAdjacentHTML("beforeend", thirdSectionComponent());
+    
+    countingUp();
     cursorHandler();
 
     scrollmanagingtwo()
